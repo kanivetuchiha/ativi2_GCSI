e5670dfee5d3aac3e9fa0714531a604acf96c447
kanivetuchiha <erickazambuja.ms@gmail.com>
2025-09-30 15:10:18 -0300

    Revert "gerenciamento de estoque"
    This reverts commit 35f8ff6a31c11c5a5d88d77601ccbae1e8883c97.

--- routes.js
+++
@@ -1,9 +0,0 @@
-import controller from './controller.js';
-import routes from 'express';
-
-const router = routes.Router();
-
-router.get('/items', controller.getItems);
-router.post('/items', controller.createItem);
-router.put('/items/:id', controller.updateItem);
-router.delete('/items/:id', controller.deleteItem);
